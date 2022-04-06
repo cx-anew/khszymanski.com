2046abf4c758e76de654678b0880c22790ad2a7f
CX-anew <h.szymanski@cx-anew.com>
2022-04-06 23:58:41 +0200

    Menu

---
+++ inc/nav.php
@@ -0,0 +1,5 @@
+<nav class="navbar fixed-bottom navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="../img/hero_2.png" alt="Signature" class="h-auto"></a>
+    </div>
+</nav>
